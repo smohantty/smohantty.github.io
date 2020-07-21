ead572f3fb17ee5d367f9a29c2235f00a156bb56
Subhransu Mohanty <sub.mohanty@samsung.com>
2020-07-21 13:38:45 +0900

    added 1st draft of lottie player

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-//IIFE
+//IIFE setup function.
 (function () {
-    console.log("setup called ")
     var head = document.head;
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -8,12 +7,48 @@
     head.appendChild(script);
 
     script.onload = _ => {
-      console.log("setup1 called ");
       Module.onRuntimeInitialized = _ => {
-        console.log("setup1 called ");
-        var instance = new Module.RlottieWasm();
-        alert(instance.frames());
+        var player = new Player();
       };
     };
 })();
 
+
+class Player
+{
+    layout() {
+      var width = document.getElementById("p-content").clientWidth;
+      var height = document.getElementById("p-content").clientHeight;
+      var size = width;
+      if (width < height)
+        size = width;
+      else
+        size = height;
+      size = size-8;
+
+      document.getElementById("p-canvas").width  = size;
+      document.getElementById("p-canvas").height  = size;
+    }
+    render(frame)
+    {
+        var canvas = document.getElementById('p-canvas');
+        var buffer = this.rlottie.render(frame, canvas.width, canvas.height)
+        var imageData = new ImageData(Uint8ClampedArray.from(buffer), canvas.width, canvas.height);
+        canvas.getContext('2d').putImageData(imageData, 0, 0);
+    }
+    tick()
+    {
+        this.render(this.frame++);
+        if (this.frame >= this.frames) this.frame = 0;
+
+        window.requestAnimationFrame(()=>{ this.tick();});
+    }
+    constructor()
+    {
+        this.layout();
+        this.rlottie = new Module.RlottieWasm();
+        this.frames = this.rlottie.frames();
+        this.frame = 0;
+        window.requestAnimationFrame(()=>{ this.tick();});
+    }
+}
